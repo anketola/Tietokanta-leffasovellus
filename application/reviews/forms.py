eb8e5e33db5247252a9ca9e95cf1d27a08a83b68
Antti Ketola <antti@anttiketola.com>
2019-06-13 12:51:50 +0300

    Arvosteluformin päivitys

--- application/reviews/forms.py
+++ application/reviews/forms.py
@@ -2,8 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import RadioField, TextAreaField, validators
 
 class ReviewForm(FlaskForm):
-    rating = RadioField("Arvosana", coerce=int, choices=[(1, "1"), (2, "2"), (3, "3"), (4, "4"), (5, "5")], validators=[validators.Required()])
-    reviewtext = TextAreaField("Arvostelun teksti", [validators.Length(min=2, max=144, message="Kentän pituuden tulee olla %(min)d - %(max)d merkkiä.")])
+    rating = RadioField("Arvosana", coerce=int, choices=[(1, "1"), (2, "2"), (3, "3"), (4, "4"), (5, "5")], validators=[validators.Required(message="Valitse arvosana elokuvalle")], render_kw={"class": "form-control"})
+    reviewtext = TextAreaField("Arvostelun teksti", [validators.Length(min=2, max=144, message="Arvostelun pituuden tulee olla %(min)d - %(max)d merkkiä.")], render_kw={"class": "form-control", "cols": "30", "rows": "10"})
 
     class Meta:
         csrf = False
