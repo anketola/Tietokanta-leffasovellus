dc9303c55d30e0dd99d5a963f8f6073f82b4ff8c
Antti Ketola <antti@anttiketola.com>
2019-06-15 16:04:00 +0300

    Paremmat formit elokuvien lisäykselle ja editoinnille

--- application/movies/forms.py
+++ application/movies/forms.py
@@ -2,19 +2,19 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SelectMultipleField, IntegerField, TextAreaField, validators
 
 class MovieForm(FlaskForm):
-    name = StringField("Elokuvan nimi", [validators.Length(min=1, max=144, message="Elokuvan nimi voi olla pitudeltaan %(min)d - %(max)d merkkiä.")])
-    released = IntegerField("Julkaisuvuosi", [validators.NumberRange(min=1800, max=2100, message="Kentän arvon tulee olla välillä %(min)s - %(max)s.")])
-    description = TextAreaField("Kuvaus", [validators.Length(min=2, max=1000, message="Kuvauksen tulee olla pituudeltaan %(min)d - %(max)d merkkiä.")])
-    category = SelectMultipleField('Kategoriat', [validators.DataRequired()], coerce=int)
+    name = StringField("Elokuvan nimi", [validators.Length(min=1, max=144, message="Elokuvan nimen tulee olla pitudeltaan %(min)d - %(max)d merkkiä.")], render_kw={"class": "form-control"})
+    released = IntegerField("Julkaisuvuosi", [validators.NumberRange(min=1800, max=2100, message="Kentän arvon tulee olla välillä %(min)s - %(max)s.")], render_kw={"class": "form-control"})
+    description = TextAreaField("Kuvaus", [validators.Length(min=2, max=1000, message="Kuvauksen tulee olla pituudeltaan %(min)d - %(max)d merkkiä.")], render_kw={"class": "form-control", "cols": "30", "rows": "7"})
+    category = SelectMultipleField('Kategoriat', [validators.DataRequired()], coerce=int, render_kw={"class": "form-control"})
 
     class Meta:
         csrf = False
 
 class EditMovieForm(FlaskForm):
-    name = StringField("Elokuvan nimi", [validators.Length(min=1, max=144, message="Kentän pituuden tulee olla %(min)d - %(max)d merkkiä.")])
-    released = IntegerField("Julkaisuvuosi", [validators.NumberRange(min=1800, max=2100, message="Kentän arvon tulee olla välillä %(min)s - %(max)s.")])
-    description = TextAreaField("Kuvaus", [validators.Length(min=2, max=1000, message="Kentän pituuden tulee olla %(min)d - %(max)d merkkiä.")])
-    category = SelectMultipleField('Kategoriat', [validators.DataRequired()], coerce=int)
+    name = StringField("Elokuvan nimi", [validators.Length(min=1, max=144, message="Kentän pituuden tulee olla %(min)d - %(max)d merkkiä.")], render_kw={"class": "form-control"})
+    released = IntegerField("Julkaisuvuosi", [validators.NumberRange(min=1800, max=2100, message="Kentän arvon tulee olla välillä %(min)s - %(max)s.")], render_kw={"class": "form-control"})
+    description = TextAreaField("Kuvaus", [validators.Length(min=2, max=1000, message="Kentän pituuden tulee olla %(min)d - %(max)d merkkiä.")], render_kw={"class": "form-control", "cols": "30", "rows": "7"})
+    category = SelectMultipleField('Kategoriat', [validators.DataRequired()], coerce=int, render_kw={"class": "form-control"})
 
     class Meta:
         csrf = False
