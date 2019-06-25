75f85e4eec8b68ea49b4fd0cfeedf92be5f69387
Antti Ketola <antti@anttiketola.com>
2019-06-25 14:22:13 +0300

    Lisätty helpottavia ohjeita suoraan sovellukseen

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -9,8 +9,8 @@ class LoginForm(FlaskForm):
         csrf = False
 
 class RegisterForm(FlaskForm):
-    username = StringField("Käyttäjänimi", [validators.Length(min=5, max=20, message="Käyttäjänimen pituuden tulee olla %(min)d - %(max)d merkkiä.")], render_kw={"class": "form-control"})
-    password = PasswordField("Salasana", [validators.Length(min=5, max=20, message="Salasanan pituuden tulee olla %(min)d - %(max)d merkkiä.")], render_kw={"class": "form-control"})
+    username = StringField("Käyttäjänimi", [validators.Length(min=5, max=20, message="Käyttäjänimen pituuden tulee olla %(min)d - %(max)d merkkiä.")], render_kw={"class": "form-control", "aria-describedby": "usernameHelp"})
+    password = PasswordField("Salasana", [validators.Length(min=5, max=20, message="Salasanan pituuden tulee olla %(min)d - %(max)d merkkiä.")], render_kw={"class": "form-control", "aria-describedby": "passwordHelp"})
   
     class Meta:
         csrf = False
